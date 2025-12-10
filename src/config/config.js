bd3e37491a3cda22a19ff42d042595062f87116a
mohinimraut7 <mohinimraut7@gmail.com>
2025-12-10 18:34:29 +0530

    new url push

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
-export const baseUrl='http://localhost:5000/api';
-export const billBaseUrl = 'http://localhost:5000';
-export const fileBaseUrl = 'http://localhost:5000';
+// export const baseUrl='http://localhost:5000/api';
+// export const billBaseUrl = 'http://localhost:5000';
+// export const fileBaseUrl = 'http://localhost:5000';
+
 export const  baseUrlEmail = 'https://thanepolice.onrender.com/api';
 
-// export const baseUrl='https://thanepolice.onrender.com/api';
-// export const billBaseUrl = 'https://thanepolice.onrender.com';
-// export const fileBaseUrl = 'https://thanepolice.onrender.com';
+export const baseUrl='https://thanepolice.onrender.com/api';
+export const billBaseUrl = 'https://thanepolice.onrender.com';
+export const fileBaseUrl = 'https://thanepolice.onrender.com';
 
