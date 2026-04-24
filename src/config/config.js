4619dd5f361c85d161b92177c452710d5f91f1c8
mohinimraut7 <mohinimraut7@gmail.com>
2026-04-24 19:21:25 +0530

    rm 24apr

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,23 @@
-export const baseUrl='http://localhost:5000/api';
-export const billBaseUrl = 'http://localhost:5000';
-export const fileBaseUrl = 'http://localhost:5000';
-export const  baseUrlEmail = 'http://localhost:5000/api';
+
+
+// export const baseUrl='http://localhost:5000/api';
+// export const billBaseUrl = 'http://localhost:5000';
+// export const  baseUrlEmail = 'http://localhost:5000/api';
+// export const fileBaseUrl = 'http://localhost:5000';
 
 
 // export const  baseUrlEmail = 'https://thanepolice.onrender.com/api';
 // export const baseUrl='https://thanepolice.onrender.com/api';
 // export const billBaseUrl = 'https://thanepolice.onrender.com';
 // export const fileBaseUrl = 'https://thanepolice.onrender.com';
+
+
+
+export const  baseUrlEmail = 'https://thanepolicerural.saavi.co.in/api';
+export const baseUrl='https://thanepolicerural.saavi.co.in/api';
+export const billBaseUrl ='https://thanepolicerural.saavi.co.in'
+export const fileBaseUrl ='https://thanepolicerural.saavi.co.in'
+
+
+
 
