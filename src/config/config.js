ddb52685671ab96985e79b0cd2cb5584b55ae30e
mohinimraut7 <mohinimraut7@gmail.com>
2026-04-24 17:53:01 +0530

    24 apr 2026 changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
-// export const baseUrl='http://localhost:5000/api';
-// export const billBaseUrl = 'http://localhost:5000';
-// export const fileBaseUrl = 'http://localhost:5000';
+export const baseUrl='http://localhost:5000/api';
+export const billBaseUrl = 'http://localhost:5000';
+export const fileBaseUrl = 'http://localhost:5000';
+export const  baseUrlEmail = 'http://localhost:5000/api';
 
-export const  baseUrlEmail = 'https://thanepolice.onrender.com/api';
 
-export const baseUrl='https://thanepolice.onrender.com/api';
-export const billBaseUrl = 'https://thanepolice.onrender.com';
-export const fileBaseUrl = 'https://thanepolice.onrender.com';
+// export const  baseUrlEmail = 'https://thanepolice.onrender.com/api';
+// export const baseUrl='https://thanepolice.onrender.com/api';
+// export const billBaseUrl = 'https://thanepolice.onrender.com';
+// export const fileBaseUrl = 'https://thanepolice.onrender.com';
 
